b24d2da924b2208df35738f25ba932a04a1b25c3
Andrew Robillard <Arobilla@hotmail.com>
2018-09-10 01:58:08 -0400

    add Angle class

---
+++ AngularAdditionMonad/Angle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AngularAdditionMonad
+{
+    public struct Angle
+    {
+        private readonly decimal degrees;
+
+        private Angle(decimal degrees)
+        {
+            this.degrees = degrees % 360;
+            if (this.degrees < 0)
+                this.degrees += 360m;
+        }
+
+        public static Angle FromDegrees(decimal degrees) => 
+            new Angle(degrees);
+
+        public static Angle FromRadians(double radians) =>
+            new Angle((decimal)((180D / Math.PI) * radians));
+
+        public Angle Add(Angle other) =>
+            new Angle(this.degrees + other.degrees);
+
+        public readonly static Angle Identity = new Angle(0);
+
+        public override bool Equals(object obj) =>
+            obj is Angle angle
+                ? angle.degrees == this.degrees
+                : base.Equals(obj);
+
+        public override int GetHashCode() =>
+            this.degrees.GetHashCode();
+
+        public static bool operator ==(Angle x, Angle y) =>
+            x.Equals(y);
+
+        public static bool operator !=(Angle x, Angle y) =>
+            !x.Equals(y);
+    }
+}
