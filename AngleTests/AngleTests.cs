770f110910a259c7181e55958b0ef53e3c57fd1d
Andrew Robillard <Arobilla@hotmail.com>
2018-09-10 02:39:47 -0400

    add tests for real

---
+++ AngleTests/AngleTests.cs
@@ -0,0 +1,31 @@
+﻿using AngularAdditionMonad;
+using FsCheck;
+using FsCheck.Xunit;
+using Xunit;
+
+namespace Tests
+{
+    public class AngleTests
+    {
+        // ToDo
+        //public static Gen<Angle> AngleHelper()
+        //{
+
+        //}
+
+        [Property(QuietOnSuccess = true)]
+        public void AddIsAssociative(Angle x, Angle y, Angle z)
+        {
+            Assert.Equal(
+                x.Add(y).Add(z),
+                x.Add(y.Add(z)));
+        }
+
+        [Property(QuietOnSuccess = true)]
+        public void AddHasIdentity(Angle x)
+        {
+            Assert.Equal(x, Angle.Identity.Add(x));
+            Assert.Equal(x, x.Add(Angle.Identity));
+        }
+    }
+}
